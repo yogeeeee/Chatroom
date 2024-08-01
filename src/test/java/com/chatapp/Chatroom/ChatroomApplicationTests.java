73cc78d6ad69000d587a3772916b1a0012a5edc3
yogeeeee <139490157+yogeeeee@users.noreply.github.com>
2024-08-01 10:13:28 +0530

    first commit

---
+++ src/test/java/com/chatapp/Chatroom/ChatroomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatapp.Chatroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatroomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
